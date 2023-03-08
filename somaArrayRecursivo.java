537bb41cc86f2bbb7a45fcae35c1f75c1230888d
felipelopes02 <100000487+felipelopes02@users.noreply.github.com>
2023-03-08 19:42:07 -0300

    Update somaArrayRecursivo.java

--- somaArrayRecursivo.java
+++ somaArrayRecursivo.java
@@ -5,3 +5,9 @@ public static int somaArrayRecursivo(int[] arr, int index) {
         return arr[index] + somaArrayRecursivo(arr, index + 1);
     }
 }
+
+int[] arr = {1, 2, 3, 4, 5};
+int index = 2;
+int resultado = somaArrayRecursivo(arr, index);
+System.out.println(resultado); // 12 (soma dos elementos 3, 4 e 5)
+
